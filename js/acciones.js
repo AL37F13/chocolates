77223535488e86e17a3c9ae22ce13c80c6554506
AL37F13 <lucero.rc.16@gmail.com>
2015-12-05 14:12:00 -0600

    nueve

--- js/acciones.js
+++ js/acciones.js
@@ -11,7 +11,7 @@ $('#btnlistado').on('tap',function(){
 		alert(respuesta);
 		$('#contenido').empty();
 		for(var $x=0; $x<producto.chocolates.length;$x++)
-		{$('#contenido').append('<div><div align="center" style="border:3px solid #963;"><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].claveChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$X].nombreChocolate +'</h1></div><div style=" display:inline-block;"><img src="#"></div></div><div align="center" style="border:3px solid #963;"><div><h1>'+ producto.chocolates[$X].tipoChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$X].saborChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$X].marcaChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$X].descripcionChocolate +'</h1></div></div></div>');}
+		{$('#contenido').append('<div><div align="center" style="border:3px solid #963;"><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].claveChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].nombreChocolate +'</h1></div><div style=" display:inline-block;"><img src="#"></div></div><div align="center" style="border:3px solid #963;"><div><h1>'+ producto.chocolates[$x].tipoChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].saborChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].marcaChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].descripcionChocolate +'</h1></div></div></div>');}
 		
 		
 		$(':mobile-pagecontainer').pagecontainer('change','#lista',{transition: 'pop'});
