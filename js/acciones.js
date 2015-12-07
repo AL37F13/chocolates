239313460c23513792e230818a18de9c6e2368ab
AL37F13 <lucero.rc.16@gmail.com>
2015-12-07 11:01:01 -0600

    once

--- js/acciones.js
+++ js/acciones.js
@@ -20,5 +20,26 @@ $('#btnlistado').on('tap',function(){
 		}
 			});//cierra el ajax
 			});//cierra el tap
+			
+			
+			$('#btnbuscar').on('tap',function(){
+	$.ajax({
+		type:"POST",
+		url:"http://192.168.1.162/proyecto/buscar.php",
+		data:"nombre=" + $('#txtnombre').val(),
+		error: function(){alert("error en conexion");},
+		success: function(respuesta){var producto = JSON.parse(respuesta);
+		alert(respuesta);
+		$('#contenido').empty();
+		for(var $x=0; $x<producto.chocolates.length;$x++)
+		{$('#contenido').append('<div><div align="center" style="border:3px solid #963;"><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].claveChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].nombreChocolate +'</h1></div><div style=" display:inline-block;"><img src="http://192.168.1.162/proyecto/recursos/imagenes/fotos/chocolate'+producto.chocolates[$x].claveChocolate+'.jpg"></div></div><div align="center" style="border:3px solid #963;"><div><h1>'+ producto.chocolates[$x].tipoChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].saborChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].marcaChocolate +'</h1></div><div style=" display:inline-block;"><h1>'+ producto.chocolates[$x].descripcionChocolate +'</h1></div></div></div>');}
+		
+		
+		$(':mobile-pagecontainer').pagecontainer('change','#lista',{transition: 'pop'});
+			
+			
+		}
+			});//cierra el ajax
+			});//cierra el tap
 }); //cierra el deviceready
 });// cierra el .ready
